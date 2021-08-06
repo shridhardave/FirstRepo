ca269ed7b2215f222bed88a1b824ca3fcc86ef66
shridhardave <dave.shridhardave@gmail.com>
2021-08-06 15:29:46 -0400

    Task 2

---
+++ Task2.sh
@@ -0,0 +1,8 @@
+cd /opt/cron-test
+sudo crontag 
+sudo git clone https://github.com/shridhardave/FirstRepo.git /opt/cron-test
+sudo chmod +x /opt/script.sh
+# echo "0 0 15 ? * * rm -f /tmp/root/*" >> /var/spool/cron/root
+sudo git clone https://github.com/shridhardave/FirstRepo.git /opt/cron-test
+$ git log -5 main --pretty=format:"%an" --name-only >> gitlog_.txt
+rm -rf FirstRepo.git
